d6cc1326e0b1a81ab852730eefd8921a7d5c2ddb
Harsh Pandey <harshcse7571@gmail.com>
2025-11-06 23:28:46 +0530

    Added a Basic Table

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Basictable from "./component/Basictable";
+
+function App() {
+  return (
+    <div>
+      <Basictable />
+    </div>
+  );
+}
+
+export default App;
